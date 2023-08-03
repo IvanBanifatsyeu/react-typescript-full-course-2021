568eb21e972437cd0b6eb31007c5ac04579a18c4
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-08-03 22:45:54 +0300

    create alias for git

---
+++ src/components/Card.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface CardProps {
+    width: string,
+    height: string
+}
+
+
+const Card = ({width, height}: CardProps) => {
+    return (
+        <div style={{width, height, backgroundColor: 'grey'}}>
+            
+        </div>
+    );
+};
+
+export default Card;
